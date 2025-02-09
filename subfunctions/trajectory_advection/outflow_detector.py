6ebc1692e5dda597b4ae332ce6ad3509416ae606
llupumo <38807372+llupumo@users.noreply.github.com>
2025-02-09 19:35:00 +0100

    Delete subfunctions directory
    loading an updated version with new functions that used to be in the Fmap.py. I have also remove the time array in the similarity matrix function

--- subfunctions/trajectory_advection/outflow_detector.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-def outflow_detector(IC,latmin,latmax,lonmin,lonmax):
-    mask = (IC[0, :] >= latmin) & (IC[0, :] <= latmax) & (IC[1, :] >= lonmin) & (IC[1, :] <= lonmax)
-    print("The number of trajectories leaving the domain is: "+str(np.sum(mask==True)))
-    return IC[:, ~mask]
